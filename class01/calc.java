bf5f69c1938d39061e49e743d681a854c89180da
Giridher Sadineni <giridher@hotmail.com>
2019-01-28 18:34:59 +0530

    added calc program

---
+++ class01/calc.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class calc{
+    public static void main(String args[]) {
+        System.out.println("The sum of 38+65 numbers is"+add(38,65) );
+
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+}
