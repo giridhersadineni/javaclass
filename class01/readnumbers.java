7ad7a720a8af1c63af3b9e675be8b02d56f1f8eb
Giridher Sadineni <giridher@hotmail.com>
2019-01-28 18:47:38 +0530

    added program

---
+++ class01/readnumbers.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Scanner;
+class readnumbers{
+    public static void main(String args[]) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter a number");
+            int n=sc.nextInt();
+            System.out.println("enter another number");
+            int m=sc.nextInt();
+            int sum=n+m;
+            System.out.println("The sum of both numbers is "+sum);
+
+
+
+    }
+   
+}
